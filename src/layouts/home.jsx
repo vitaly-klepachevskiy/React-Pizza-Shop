26946d7621e3b6c46991d93bf635e623e0b0795c
Vitaly Klepachevskiy <vitaly.klepachevskiy@gmail.com>
2023-01-04 18:11:34 +0400

    Web: pagination is deleted

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -4,7 +4,7 @@ import { setCategoryId } from '../redux/slices/filterSlice';
 import Categories from '../components/categories';
 import Sort from '../components/sort';
 import { Skeleton, PizzaBlock } from '../components/pizzaBlock';
-import { Pagination } from '../components/pagination';
+// import { Pagination } from '../components/pagination';
 import { SearchContext } from '../App';
 
 const Home = () => {
@@ -15,7 +15,7 @@ const Home = () => {
 
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(0);
+  // const [currentPage, setCurrentPage] = useState(0);
 
   const onClickCategory = (id) => {
     dispatch(setCategoryId(id));
@@ -28,9 +28,7 @@ const Home = () => {
   useEffect(() => {
     setIsLoading(true);
     fetch(
-      `https://63a83f85100b7737b97a80c8.mockapi.io/items?page=${
-        currentPage + 1
-      }&limit=4&${category}&sortBy=${sortBy}&order=${order}`
+      `https://63a83f85100b7737b97a80c8.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -38,7 +36,7 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, category, sortBy, order, currentPage]);
+  }, [categoryId, sortType, category, sortBy, order]);
 
   const pizzas = items
     .filter((obj) =>
@@ -57,7 +55,7 @@ const Home = () => {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeleton : pizzas}</div>
-      <Pagination onChangePage={(pageIndex) => setCurrentPage(pageIndex)} />
+      {/* <Pagination onChangePage={(pageIndex) => setCurrentPage(pageIndex)} /> */}
     </div>
   );
 };
